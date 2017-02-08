4e6be68360b5bda0befb4a8c057f33d3eadb64f8
admin <cmeshnulya@mail.ru>
2017-02-08 16:06:46 +0300

    New text in view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.utils import timezone
+from django.http import HttpResponse
+from django.template import loader
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.all()
+    template = loader.get_template('blog/post_list.html')
+    context = {
+        'posts': posts,
+}
+    return HttpResponse(template.render(context, request))
