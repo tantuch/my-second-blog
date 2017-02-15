63a3211c00de796120583c125ac027509571254a
admin <cmeshnulya@mail.ru>
2017-02-15 18:29:36 +0300

    Added view and template and css

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +11,7 @@ def post_list(request):
         'posts': posts,
 }
     return HttpResponse(template.render(context, request))
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post})
