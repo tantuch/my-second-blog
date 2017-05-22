060a44cfa93865b9f6d8674cf5d133e31033062d
admin <cmeshnulya@mail.ru>
2017-05-22 23:00:20 +0300

    Fixed error of tab.

--- blog/models.py
+++ blog/models.py
@@ -8,14 +8,14 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-
+    
     def publish(self):
         self.published_date = timezone.now()
         self.save()
         
     def approved_comments(self):
-		return self.comments.filter(approved_comment=True)
-
+        return self.comments.filter(approved_comment=True)
+	
     def __str__(self):
         return self.title
         
