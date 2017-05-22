8508c016a1fe7abf2f1523c9f2de53fe98122742
admin <cmeshnulya@mail.ru>
2017-05-22 22:32:41 +0300

    Add unpublished posts; publish, delete button. Authorizing(welcoming). Create comment blog model.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.template import loader
-from .models import Post
+from .models import Post, Comment
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
@@ -43,3 +43,32 @@ def post_edit(request, pk):
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+	
+@login_required
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+	
+@login_required
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+	
+@login_required
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
+	
+@login_required
+def comment_approve(request, pk):
+	comment = get_object_or_404(Comment, pk=pk)
+	comment.approve()
+	return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+	comment = get_object_or_404(Comment, pk=pk)
+	comment.delete()
+	return redirect('post_detail', pk=comment.post.pk)
